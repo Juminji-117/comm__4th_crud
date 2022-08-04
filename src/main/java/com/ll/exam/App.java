ac77411947b26474a2f082f20bb7c8411a68f249
Ju, Minji <alswl0w0@gmail.com>
2022-08-04 11:04:02 +0900

    setting

---
+++ src/main/java/com/ll/exam/App.java
@@ -0,0 +1,10 @@
+package com.ll.exam;
+
+public class App {
+    public static final String BASE_PACKAGE_PATH = "com.ll.exam";
+    public static final String DB_HOST = "localhost";
+    public static final int DB_PORT = 3306;
+    public static final String DB_ID = "user2";
+    public static final String DB_PASSWORD = "test1234";
+    public static final String DB_NAME = "blog";
+}
